32b85118587e20aba2c3eb1a3f0138c9a175ce88
Ashley Coleman <ashley78@ksu.edu>
2017-10-17 20:17:00 -0500

    post requests

--- public/bundle.js
+++ public/bundle.js
@@ -1,18 +1,50 @@
 var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
+    this.get = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        
+        xhr.addEventListener('readystatechange', function() {
+            if (this.readyState === 4 && this.status == 200) {
+                callback(this.responseText);
+            }
+        });
 
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
+        xhr.open('GET', url);            
+        xhr.send(null);
     }
+
+    this.post = function(url, params, callback) {
+        var data = new FormData();
+
+        Object.entries(params).forEach(function([key, value]) { 
+            data.append(key, value);
+        });
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4 && this.status == 200) {
+                callback(this.responseText);
+            }
+        }); 
+
+        xhr.open("POST", url);
+        xhr.setRequestHeader("accept", "application/json");
+        
+        xhr.send(data);
+    } 
 }
 
 var client = new HttpClient();
 client.get('/api/v1/event', function(response) {
     // do something with response
-    alert(response);
+    console.log(response);
+});
+
+var params = {
+    owner: "Ash",
+    identifier: "asdf123"
+};
+
+client.post('/api/v1/card', params, function(response){
+    console.log(response)
 });
