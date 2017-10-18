b23ee55ed44ace79346b17c3249a31beab7c236b
Ashley Coleman <ashley78@ksu.edu>
2017-10-17 22:24:16 -0500

    button

--- resources/views/main.php
+++ resources/views/main.php
@@ -2,6 +2,7 @@
     <body>
         <select id="hackathon-selection"></select>
         <select id="event-selection"></select>
+        <button id="begin-swiping">Begin Swiping</button>
         <script src="./bundle.js"></script>
     </body>
 </html>
